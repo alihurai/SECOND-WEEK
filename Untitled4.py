bf22fd2ede3f8b57cc036d9fc68d77214e31836e
alihurai <60705638+alihurai@users.noreply.github.com>
2020-02-09 18:38:49 +0500

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+i=9
+
+
+# In[1]:
+
+
+y=9+9
+
+
+# In[2]:
+
+
+print (y)
+
+
+# In[3]:
+
+
+orignal_num=90
+
+
+# In[4]:
+
+
+num_to_be_added=7
+
+
+# In[9]:
+
+
+new_num = orignal_num + num_to_be_added
+
+
+# In[11]:
+
+
+print (new_num)
+
+
+# In[12]:
+
+
+weight = 180
+
+
+# In[13]:
+
+
+height =160
+
+
+# In[14]:
+
+
+total_difference = weight/height
+
+
+# In[17]:
+
+
+print (total_difference)
+
+
+# In[22]:
+
+
+table=100
+
+
+# In[24]:
+
+
+chair=80
+
+
+# In[26]:
+
+
+total_table+chair
+
+
+# In[27]:
+
+
+boys=800
+
+
+# In[32]:
+
+
+girls=735
+
+
+# In[41]:
+
+
+total_boys+girls
+
+
+# In[42]:
+
+
+original_num=110
+
+
+# In[43]:
+
+
+num_to_be_added=59
+
+
+# In[44]:
+
+
+original_num + num_to_be_added
+
+
+# In[45]:
+
+
+my_num=5
+
+
+# In[46]:
+
+
+your_num=4
+
+
+# In[47]:
+
+
+my_num + your_num
+
+
+# In[50]:
+
+
+print =(my_num)
+
+
+# In[51]:
+
+
+my_num =5
+
+
+# In[ ]:
+
+
+orignal_num=110
+
